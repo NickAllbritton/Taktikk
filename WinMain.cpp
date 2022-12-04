5c7b0d7099662fc5f0227d57fce0a9b4b8197713
Nick Allbritton <nallbritton@sycamores.indstate.edu>
2022-12-04 14:19:04 -0500

    some setup code for now

---
+++ WinMain.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, INT nCmdShow)
+{
+    WNDCLASSEX wc = { 0 };
+    
+    // register window class
+    //
+    return 0;
+}
